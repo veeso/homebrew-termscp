9b62a06e4b72d127303012b246ab33e26d9fbade
ChristianVisintin <christian.visintin1997@gmail.com>
2020-12-06 21:31:41 +0100

    Updated package to release

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -2,7 +2,7 @@ class TermSCP < Formula
   desc "Basically, WinSCP on a terminal"
   homepage "https://github.com/ChristianVisintin/TermSCP"
   url "https://github.com/ChristianVisintin/TermSCP/releases/latest/download/termscp-mac.tar.gz"
-  sha256 "cd421098cfd22cc279a7212be7d95e903283d9eae19e358ae0aa5ba9d84f699f"
+  sha256 "e6cfba8fc70426c6159c6a80b2bba85539cde2d8fe9867191e184daf81a9a518"
   version "0.1.0"
 
   def install
