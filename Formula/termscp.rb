e6ac903d2a522785cdf3c52c476203e3ee796999
ChristianVisintin <christian.visintin1997@gmail.com>
2021-02-28 15:17:26 +0100

    termscp 0.3.3

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -2,8 +2,8 @@ class Termscp < Formula
   desc "Basically, WinSCP on a terminal"
   homepage "https://github.com/veeso/termscp"
   url "https://github.com/veeso/termscp/releases/latest/download/termscp-mac.tar.gz"
-  sha256 "84247fdf94f510b30e7c7d6957311af2b6ddfaa27b26ec10d121942663c67800"
-  version "0.3.2"
+  sha256 "5866886a0fdc4a75ece548ea64515f8e77841b9eefc0fcdd935a8e4778f14193"
+  version "0.3.3"
 
   def install
     bin.install "termscp"
