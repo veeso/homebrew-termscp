f9539f8d1d335e1e247b5108750184002608f67c
veeso <christian.visintin1997@gmail.com>
2021-08-24 15:08:52 +0200

    termscp 0.6.1

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -2,8 +2,8 @@ class Termscp < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP"
   homepage "https://github.com/veeso/termscp"
   url "https://github.com/veeso/termscp/releases/latest/download/termscp-mac.tar.gz"
-  sha256 "7df72cf5f6f7dd57ce06e8ee2baba5ab6c01e70ac61f826d80fcc1f1d4957beb"
-  version "0.6.0"
+  sha256 "8fb088d6f9d7db57fb0dbc5857600d035e5755b8d8e0a90695aea489f91fa5d0"
+  version "0.6.1"
 
   def install
     bin.install "termscp"
