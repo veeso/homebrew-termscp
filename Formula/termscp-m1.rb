c3556eec7e0e742f1945ad2f69a7f8c063a9831c
veeso <christian.visintin1997@gmail.com>
2022-04-27 09:00:34 +0200

    0.8.2

--- Formula/termscp-m1.rb
+++ Formula/termscp-m1.rb
@@ -1,9 +1,9 @@
 class TermscpM1 < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3"
   homepage "https://github.com/veeso/termscp"
-  url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.8.1-arm64-apple-darwin.tar.gz"
-  sha256 "2be304f49eb3910324969d7bb4f90292a037760665bc9c4a4063060429699e94"
-  version "0.8.1"
+  url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.8.2-arm64-apple-darwin.tar.gz"
+  sha256 "c5838390dcfbb1ad1abbaff6271ededc336a5c655fd2192713046acda1047a45"
+  version "0.8.2"
 
   def install
     bin.install "termscp"
--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -1,9 +1,9 @@
 class Termscp < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3"
   homepage "https://github.com/veeso/termscp"
-  url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.8.1-x86_64-apple-darwin.tar.gz"
-  sha256 "9678785a2a28a43e597734551b64bdbb847915d75939d60e7dc6a8826bbff17f"
-  version "0.8.1"
+  url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.8.2-x86_64-apple-darwin.tar.gz"
+  sha256 "eb904ee85abee73e342978a3b54fa26169b0e83518b35424ffc07d615c61f8e2"
+  version "0.8.2"
 
   def install
     bin.install "termscp"
