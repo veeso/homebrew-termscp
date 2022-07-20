d9070d8db588ba91da73425e5c87ba50a2774dd2
veeso <christian.visintin1997@gmail.com>
2022-07-20 12:36:27 +0200

    ssl static

--- Formula/termscp-m1.rb
+++ Formula/termscp-m1.rb
@@ -2,7 +2,7 @@ class TermscpM1 < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3"
   homepage "https://github.com/veeso/termscp"
   url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.9.0-arm64-apple-darwin.tar.gz"
-  sha256 "fe868d834ddec9e830dd3ac83b815c47aef49a70378d8ec94d3408dc942cedad"
+  sha256 "9f8fe7a0e1cdf2742560d3cd390d53b9fa3b7708456eaef584eb480df6f34653"
   version "0.9.0"
 
   def install
--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -2,10 +2,8 @@ class Termscp < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3"
   homepage "https://github.com/veeso/termscp"
   url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.9.0-x86_64-apple-darwin.tar.gz"
-  sha256 "0883646471233bf48178117bf4ba31f850d95be800916ffa2f6a7cd09f06f039"
+  sha256 "122f7021313d63d1eebc202fc8c3b85c846872e19c21a42a1cdcbad7a8100db4"
   version "0.9.0"
-  # dependencies
-  depends_on "openssl@1.1"
 
   def install
     bin.install "termscp"
