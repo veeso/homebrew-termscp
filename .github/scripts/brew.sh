df6427c7d80b447f18c5c36f1c9fc9f529c99a1a
veeso <christian.visintin1997@gmail.com>
2023-05-13 18:07:14 +0200

    fix: linux script

--- .github/scripts/brew.sh
+++ .github/scripts/brew.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-apt update
-apt install -y build-essential curl file git ruby-full
+sudo apt update
+sudo apt install -y build-essential curl file git ruby-full
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 export PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
 
-brew install --build-from-source ./Formula/termscp.rb
+sudo -H brew install --build-from-source ./Formula/termscp.rb
