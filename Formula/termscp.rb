82fd9843fde5216ca77e84c413e9f2a815e6be30
veeso <christian.visintin1997@gmail.com>
2023-05-16 18:11:38 +0200

    fix: hash

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -21,11 +21,11 @@ class Termscp < Formula
 
     on_arm do
       url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d0cd088c6432e641609f342398e6c066623d624ace8be38f4262b35f5165d328"
+      sha256 "6afc3fa19ad1f828762eae3887b8ee21adb8b7bc615017fa561f7c7dcb024f98"
     end
     on_intel do
       url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b2773ec5e67c32af2d8b34a6e2f000193114bcfe32886258986930d983a43b2b"
+      sha256 "36982c920d108249c1843971b6a3663a57de7d95a9d7c0db227bd768e2c49995"
     end
   end
 
