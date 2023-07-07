fd0f8af0c3c62fed4a6ddeeafc4c76744ef3f189
veeso <christian.visintin@veeso.dev>
2023-07-07 15:48:13 +0200

    0.12.1

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -1,17 +1,17 @@
 class Termscp < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3/SMB"
-  homepage "https://github.com/veeso/termscp"
+  homepage "https://termscp.veeso.dev/"
   license "MIT"
-  version "0.12.0"
+  version "0.12.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.0-arm64-apple-darwin.tar.gz"
-      sha256 "4e4a1bb74070cfbe1f91964c7ef3e5aaf17258d87c4d4abb74c49582d1f571aa"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-arm64-apple-darwin.tar.gz"
+      sha256 "0c3a4f7492a8fb08c944c6cdb54a54403c0bc56fd8345e509c237b8c823401af"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.0-x86_64-apple-darwin.tar.gz"
-      sha256 "d1a86027d8b6bda10f2bab5d77d57a2ca89a263809c71a7630423f82b86376ad"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-x86_64-apple-darwin.tar.gz"
+      sha256 "12fca02b6f21b5cc3f93ed04d8f8240f507df818f45a532c6be84b47730d9f40"
     end
   end
 
@@ -20,12 +20,12 @@ class Termscp < Formula
     depends_on "dbus"
 
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6afc3fa19ad1f828762eae3887b8ee21adb8b7bc615017fa561f7c7dcb024f98"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "8fedd25c97616b7f10b67031b3ec229832f00440b2f3c7423923537657789251"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "36982c920d108249c1843971b6a3663a57de7d95a9d7c0db227bd768e2c49995"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "428605ad130124858e749edc0d3221a5ca039885669f989f55c349e29c5b3a3d"
     end
   end
 
