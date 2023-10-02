b494ba3e9904f42866327f6d3f6cc03cda955cdb
veeso <christian.visintin@veeso.dev>
2023-10-02 16:51:29 +0200

    madonna

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -2,16 +2,16 @@ class Termscp < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3/SMB"
   homepage "https://termscp.veeso.dev/"
   license "MIT"
-  version "0.12.1"
+  version "0.12.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-arm64-apple-darwin.tar.gz"
-      sha256 "0c3a4f7492a8fb08c944c6cdb54a54403c0bc56fd8345e509c237b8c823401af"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.2-arm64-apple-darwin.tar.gz"
+      sha256 "242d0fe9300eee36fd49badbf441db1808dc240629b9d2b3f50007c5cf633bc0"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-x86_64-apple-darwin.tar.gz"
-      sha256 "12fca02b6f21b5cc3f93ed04d8f8240f507df818f45a532c6be84b47730d9f40"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.2-x86_64-apple-darwin.tar.gz"
+      sha256 "b74f76f15a4a7fc27ec934cf688ab15c820424c56e35a6291e1aebb683374270"
     end
   end
 
@@ -20,12 +20,12 @@ class Termscp < Formula
     depends_on "dbus"
 
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8fedd25c97616b7f10b67031b3ec229832f00440b2f3c7423923537657789251"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "2b32247e7c8da834b5970fe9abd665edf5e9f46f083c7b56e1d83634761a9ca2"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "428605ad130124858e749edc0d3221a5ca039885669f989f55c349e29c5b3a3d"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d2e21c3a92cea328f6a951f6d79df038417af230f6de29c06473ea9ed299bf95"
     end
   end
 
