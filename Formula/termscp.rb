c156d9c1caabaae6e0a44567ba86d5df49ae22c8
veeso <christian.visintin@veeso.dev>
2024-03-02 20:28:14 +0100

    fix: termscp 0.13.0

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -1,17 +1,17 @@
 class Termscp < Formula
-  desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3/SMB"
+  desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3/SMB/WebDAV"
   homepage "https://termscp.veeso.dev/"
   license "MIT"
-  version "0.12.3"
+  version "0.13.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.3-arm64-apple-darwin.tar.gz"
-      sha256 "f8fbecb7545e1ba92b39067c08f3a82b05622da07297dd312d8b7f4a2314e8da"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.13.0-arm64-apple-darwin.tar.gz"
+      sha256 "6f7537e8451cebd623a712ade9c0e88253bea11d8d1dec2b2016dc1e08bb8a64"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.3-x86_64-apple-darwin.tar.gz"
-      sha256 "14260cf55cd704ee97492fb16b498da84a689195f2858eaccf02fcd92480a07c"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.13.0-x86_64-apple-darwin.tar.gz"
+      sha256 "50fadc30790f35313eb624719c61b59e26ea59155896a2a616cd241414fafc84"
     end
   end
 
@@ -20,12 +20,12 @@ class Termscp < Formula
     depends_on "dbus"
 
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.3-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ad722f6a6b3257567e74a7f60180992eb7c352ad1b46972a99f04f2e976a9649"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.13.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a55a73e9eb65d6f786a6856aa830d6bdd71b2c4dfa48d3ea4c3feb3f70f2ce5f"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.12.3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1acb1ee897268ab0a0eae37ec9d5b6493213e2d603e7c9b2f17720e2ca2ee241"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.13.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "20d7cbf182640b333e1c30a0359c036f0e5f636ece51a4d7cdd2f04f9c8166e9"
     end
   end
 
