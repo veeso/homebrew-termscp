6bc719ef22efa24b7c4948ebc808c91bddeada14
veeso <christian.visintin@veeso.dev>
2024-10-14 16:40:03 +0200

    feat: 0.16

--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -2,16 +2,16 @@ class Termscp < Formula
   desc "A feature rich terminal file transfer and explorer with support for SCP/SFTP/FTP/S3/Kube/SMB/WebDAV"
   homepage "https://termscp.veeso.dev/"
   license "MIT"
-  version "0.15.0"
+  version "0.16.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.15.0-arm64-apple-darwin.tar.gz"
-      sha256 "fd6e954e3c7ceb6045de7ab433d17c803ffb4c8fc61835a891335a073a2dd620"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.16.0-arm64-apple-darwin.tar.gz"
+      sha256 "0ed54159d43c399504ca0d1f86121d36d77380601e9bcda9c85c756a59461f99"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.15.0-x86_64-apple-darwin.tar.gz"
-      sha256 "04e009b2c39a184da6062d326a2e8997b5de8c2fcd89ad8521f3c37b736c0b07"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.16.0-x86_64-apple-darwin.tar.gz"
+      sha256 "48bbf874feb485abb9c04c591bcf4494f7b6ff531d1a34fc2c2567b4ddbfddd8"
     end
   end
 
@@ -20,12 +20,12 @@ class Termscp < Formula
     depends_on "dbus"
 
     on_arm do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.15.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6ba36b56a8b377c2978f5aefc110b831bf3e34805c8192866251e3d16363f802"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.16.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7a42971fa705d4166824972f9c4a05d3524900d6ad28c9c0fcaf92e60be7d505"
     end
     on_intel do
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.15.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "078666686f8e4dc211a31a1e34b233b075dd778512ea9a477f4ef992e6bb7909"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.16.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "1a7263bd4a19ac208865d3bfcf50c1e4f6234892a57f013922883cb199c57962"
     end
   end
 
